e6791d88a7595f88eaaf3c21fd8415f5df7b26db
hyuma7 <96800347+hyuma7@users.noreply.github.com>
2022-09-27 13:35:38 +0900

    Add the end
    ページが終わったら抜けるように改良した。

--- LoginInstagram.py
+++ LoginInstagram.py
@@ -3,8 +3,6 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 import getpass
 
-profile = 'instagram_auto_like'
-
 def access_to_instageam(profile):
     """seleniumの起動設定を行い、起動する。
     
@@ -14,7 +12,7 @@ def access_to_instageam(profile):
     user = getpass.getuser()
     user_data_dir_profile = f"C:\\Users\\{user}\\AppData\\Local\\Google\\Chrome\\User Data"
     options = webdriver.ChromeOptions()
-    options.add_argument(r"user-data-dir=" + user_data_dir_profile + 'instagram_auto_like')
+    options.add_argument(r"user-data-dir=" + user_data_dir_profile + profile)
     driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
     driver.get('https://www.instagram.com/')
     sleep(3)
--- like_and_scroll.py
+++ like_and_scroll.py
@@ -24,11 +24,12 @@ class like_and_scroll_home():
     def wheel(self):
         """スクロール"""
         photo_text = self.driver.find_elements(By.CLASS_NAME,'_ae1h')[-1].text
-
+        count = 0
         while(True):
             self.driver.execute_script("window.scrollBy(0, 120)")
-            if self.driver.find_elements(By.CLASS_NAME,'_ae1h')[-1].text != photo_text:
-                break
+            if self.driver.find_elements(By.CLASS_NAME,'_ae1h')[-1].text != photo_text:break
+            if count >= 15: return 'end'
+            count += 1
             sleep(0.1 + random.random()*0.2)
         
 class like_and_scroll_search():
@@ -38,37 +39,43 @@ class like_and_scroll_search():
 
     def Do_Push_like(self):
         like_elem = self.driver.find_element(By.CLASS_NAME,'_aamw')
-        like_elem.find_element(By.TAG_NAME,'button').click()
-        sleep(1 + random.random())
+        if len(like_elem.find_elements(By.TAG_NAME,'div')) == 2:
+            like_elem.find_element(By.TAG_NAME,'button').click()
+            sleep(1 + random.random())
 
     def Click_first_photo(self):
         photoraw = self.driver.find_element(By.CLASS_NAME,'_ac7v')
         photoraw.find_element(By.TAG_NAME,'a').click()
-        sleep(0.5)
+        sleep(2)
 
     def move_next(self):
-        move_elem = self.driver.find_element(By.CLASS_NAME,'_aaqg')
-        move_elem.find_element(By.TAG_NAME,'button').click()
-        sleep(0.5 + random.random())
+        if len(self.driver.find_elements(By.CLASS_NAME,'_aaqg')) != 0:
+            move_elem = self.driver.find_element(By.CLASS_NAME,'_aaqg')
+            move_elem.find_element(By.TAG_NAME,'button').click()
+            sleep(1 + random.random())
+        else: return'end'
 # main article _aanc _aabd a .href #URL _aabdで複数
 
 
 if __name__ == '__main__':
     import LoginInstagram as li
     import control
-    profile = 'instagram_auto_like'
+    profile = 'instagram_auto_like_mer'
 
     driver = li.access_to_instageam(profile)
-    search_rezult = control.do_search(driver,'fxトレーダー')
+    
+    search_rezult = control.do_search(driver,'メルカリ')
     if search_rezult == 'success':
         search = like_and_scroll_search(driver)
         search.Click_first_photo()
-        for _ in range(100):
+        for _ in range(200):
             search.Do_Push_like()
-            search.move_next()
-    """ 
+            if search.move_next() == 'end':break
+    """
     insta = like_and_scroll_home(driver) # インスタンス化
     for _ in range(100):
         insta.Do_Push_like()
-        insta.wheel()
+        condition = insta.wheel()
+        if condition == 'end': 
+            break
     """
